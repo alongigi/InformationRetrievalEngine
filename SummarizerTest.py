eee8390de1cbdfc193ecd2ea0aa66f09e3962dc6
alongigi <giga@post.bgu.ac.il>
2018-05-17 20:14:07 +0300

    summarizertest

---
+++ SummarizerTest.py
@@ -0,0 +1,31 @@
+import pickle
+import unittest
+
+from Controller import Controller
+from Document import Document
+from ReadFile import ReadFile
+from Summerizer import Summerizer
+
+
+class SummarizarTest(unittest.TestCase):
+    def test_something(self):
+        c = Controller()
+        dictionary = open("./test_data/test_dict.dic", "br")
+        c.set_dictionary(pickle.load(dictionary))
+        dictionary.close()
+        cache = open("./test_data/test_cache.cch", "br")
+        c.set_cache(pickle.load(cache))
+        cache.close()
+        dicts = c.get_dictionary()
+        summerizer = Summerizer("./test_data/stop_words.txt")
+        doc_id = 'FBIS3-29'
+        file_name = dicts.docs_dict[doc_id]['file_name'].strip()
+        r = ReadFile()
+        docs = r.read_file_from_path("./test_data/corpus/", file_name)
+        doc = list(filter(lambda d: d.id == doc_id, docs))[0]
+        res = summerizer.get_importent_sentences(doc, 5)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
